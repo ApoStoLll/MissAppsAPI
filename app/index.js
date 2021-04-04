7788451cb1b141590015a0a7f347602bddd23cb6
neket <neketapostolov@gmail.com>
2021-04-05 00:24:59 +0300

    trying

--- app/index.js
+++ app/index.js
@@ -1,12 +1,31 @@
 // подключение express
 const express = require("express");
+//const { request } = require("node:http");
 // создаем объект приложения
 const app = express();
-// определяем обработчик для маршрута "/"
-app.get("/anime", function(request, response){
-     
-    // отправляем ответ
-    response.send("<h2>Привет Express!</h2>");
+
+//Создание информации о пользователе после установки прилы(на сервере)
+app.post("/install", (request, response) => {
+    response.send("request.body")
+});
+
+//Получение информации о пользователе(надо вернуть инфу)
+app.post("/install/:userId", (request, response) => {
+    response.send("request.body")
+});
+
+//Обновить пуш токен
+app.post("/install/:userId/push-token", (request, response) => {
+    response.send("request.body")
+});
+
+//Клик на пуш
+app.post("/notifications/clicked", (request, response) => {
+    response.send("request.body")
+});
+
+app.get("/test", (request, response) => {
+    response.send(request.headers)
 });
 // начинаем прослушивать подключения на 3000 порту
 app.listen(3000);
